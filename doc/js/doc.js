295a38c9e6fc4ea6825a9fa2a5b6763546f1e219
harveyprince <948841233@qq.com>
2015-04-22 00:02:26 +0800

    user and comment lock js

--- doc/js/doc.js
+++ doc/js/doc.js
@@ -7,9 +7,11 @@ $(".HP-User-List").bind("lock",function(e){
 	pos = isNaN(pos)?0:pos;
 	if(pos < 0){
 		$target.animate({left:"0"},"slow");
+		$target.find(".panel-key i").removeClass('fa-chevron-right').addClass('fa-chevron-left');
 	}else{
 		var width = $target.width();
 		$target.animate({left:(10-width)+"px"},"slow");
+		$target.find(".panel-key i").removeClass('fa-chevron-left').addClass('fa-chevron-right');
 	}
 });
 $(".HP-Comment-List").bind("lock",function(e){
@@ -18,8 +20,10 @@ $(".HP-Comment-List").bind("lock",function(e){
 	pos = isNaN(pos)?0:pos;
 	if(pos < -1){
 		$target.animate({right:"-1"},"slow");
+		$target.find(".panel-key i").removeClass('fa-chevron-left').addClass('fa-chevron-right');
 	}else{
 		var width = $target.width();
 		$target.animate({right:(10-width)+"px"},"slow");
+		$target.find(".panel-key i").removeClass('fa-chevron-right').addClass('fa-chevron-left');
 	}
 });
